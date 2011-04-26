354c391d3133d4d6514f3d662ec46511029e5d08
Carl Lerche <me@carllerche.com>
2011-04-26 11:02:28 -0700

    Add a test for keep alive requests

--- test/picard/test/server.clj
+++ test/picard/test/server.clj
@@ -43,6 +43,36 @@
         :binding nil
         :request [(includes-hdrs {"content-length" "5"}) "Hello"]))))
 
+(deftest keepalive-requests
+  (running-call-home-app
+   (http-write "GET / HTTP/1.1\r\n\r\n"
+               "GET /foo HTTP/1.1\r\n\r\n"
+               "POST /bar HTTP/1.1\r\n"
+               "connection: close\r\n\r\n")
+   (is (next-msgs
+        :binding nil
+        :request [(includes-hdrs {:request-method "GET" :path-info "/"}) nil]
+        :done    nil
+        :binding nil
+        :request [(includes-hdrs {:request-method "GET" :path-info "/foo"}) nil]
+        :done    nil
+        :binding nil
+        :request [(includes-hdrs {:request-method "POST" :path-info "/bar"
+                                  "connection" "close"}) nil]))
+   (is (received-response
+        "HTTP/1.1 200 OK\r\n"
+        "content-type: text/plain\r\n"
+        "content-length: 5\r\n\r\n"
+        "Hello"
+        "HTTP/1.1 200 OK\r\n"
+        "content-type: text/plain\r\n"
+        "content-length: 5\r\n\r\n"
+        "Hello"
+        "HTTP/1.1 200 OK\r\n"
+        "content-type: text/plain\r\n"
+        "content-length: 5\r\n\r\n"
+        "Hello"))))
+
 (deftest request-callback-happens-before-body-is-recieved
   (running-call-home-app
    (http-write "POST / HTTP/1.1\r\n"
