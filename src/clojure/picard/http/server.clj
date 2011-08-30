1a03e96dad22c2aa030e1e08f1c9ae48ba830a9d
Carl Lerche <me@carllerche.com>
2011-08-30 15:33:28 -0700

    Getting close to finishing the new HTTP protocol middleware

--- src/clojure/picard/http/server.clj
+++ src/clojure/picard/http/server.clj
@@ -1,14 +1,22 @@
 (ns picard.http.server
   (:use picard.http.core))
 
+;; TODO:
+;; * Count the bytes in the request body
+
 (declare
  handle-request
  handle-response
  awaiting-100-continue
  waiting-for-response)
 
+(defrecord ConnectionState
+    [upstream])
+
 (defrecord ExchangeState
     [connection
+     upstream
+     downstream
      next-up-fn
      next-dn-fn
      keepalive?
@@ -17,9 +25,11 @@
      responded?])
 
 (defn- initial-exchange-state
-  [conn]
+  [conn downstream]
   (ExchangeState.
    conn
+   nil
+   downstream
    handle-request
    handle-response
    true
@@ -29,20 +39,76 @@
 
 (defn- awaiting-100-continue?
   [current-state]
-  `(= awaiting-100-continue (.next-up-fn ~current-state)))
+  (= awaiting-100-continue (.next-up-fn current-state)))
 
 (defn- awaiting-response?
   [current-state]
-  `(= waiting-for-response (.next-up-fn ~current-state)))
+  (= waiting-for-response (.next-up-fn current-state)))
 
-(defn- finalize-exchange
-  [state current-state])
+(defn- exchange-finished?
+  [current-state]
+  (and (.responded? current-state)
+       (or (awaiting-response? current-state)
+           (awaiting-100-continue? current-state))))
+
+(defn- maybe-finalize-exchange
+  [current-state]
+  (when (exchange-finished? current-state)
+    (let [conn     (.conn current-state)
+          upstream (.upstream current-state)]
+
+      ;; The current exchange is complete, so remove it from
+      ;; the connection state.
+      (swap! conn #(dissoc % :upstream))
+
+      ;; Send an upstream event that indicates the HTTP exchange
+      ;; is complete.
+      (upstream :done nil))))
+
+(defn- maybe-close-connection
+  [current-state]
+  (when-not (.keepalive? current-state)
+    ((.downstream current-state) :close nil)))
 
 (defn- stream-or-finalize-response
-  [])
+  [state evt chunk current-state]
+  (when-not (= :body evt)
+    (throw (Exception. "Expecting a :body event")))
+
+  (swap-then!
+   state
+   (fn [current-state]
+     (if chunk
+       (let [bytes-sent     (.bytes-to-send current-state)
+             bytes-expected (.bytes-expected current-state)
+             bytes-to-send  (+ bytes-sent (chunk-size chunk))
+             responded?     (= bytes-expected bytes-to-send)]
+         (assoc current-state
+           :bytes-to-send bytes-to-send
+           :responded?    responded?
+           :next-dn-fn    (when-not responded? stream-or-finalize-request)))
+       ;; This is the final chunk
+       ;; TODO: Check that the content-length is correct
+       (assoc current-state
+         :responded? true
+         :next-dn-fn nil)))
+   (fn [current-state]
+     (let [responded? (.responded? current-state)
+           chunked?   (.chunked? current-state)
+           downstream (.downstream current-state)]
+       ;; It only makes sense to check if the exchange needs to be finalized
+       ;; when the response is complete.
+       (when responded?
+         (maybe-finalize-exchange current-state))
+
+       (cond
+        chunk    (downstream :message (chunk->netty-chunk chunk))
+        chunked? (downstream :message HttpChunk/LAST_CHUNK))
+
+       (maybe-close-connection current-state)))))
 
 (defn- handle-response
-  [state next-up evt val current-state]
+  [state evt val current-state]
   (when-not (= :response evt)
     (throw (Exception. "Expecting a :response event")))
 
@@ -89,43 +155,46 @@
                                      (= (hdrs "transfer-encoding") "chunked")
                                      (not (status-expects-body? status))))))))
      (fn [current-state]
-       (throw (Exception. "Not implemented"))))))
+       (let [downstream (.downstream current-state)
+             message    (response->netty-response status hdrs body)]
+         (maybe-finalize-exchange current-state)
+         (downstream :message message)
+         (maybe-close-connection current-state))))))
 
 (defn- waiting-for-response
   [_ _ _ _ _]
   (throw (Exception. "Not expecting a message right now.")))
 
 (defn- stream-or-finalize-request
-  [state next-up evt val current-state]
-  ;; TODO: Handle the case when (not= :body evt)
+  [state evt val current-state]
   (let [chunk (normalize-chunk val)]
     (if chunk
-      (next-up :body chunk)
+      ((.upstream current-state) :body chunk)
       (swap-then!
        state
        #(assoc % :next-up-fn waiting-for-response)
        (fn [current-state]
-         (next-up :body nil)
-         (finalize-exchange state current-state))))))
+         (maybe-finalize-exchange current-state)
+         ((.upstream current-state) :body nil)
+         (maybe-close-connection current-state))))))
 
 (defn- awaiting-100-continue
-  [state next-up evt val current-state]
+  [state evt val current-state]
   (swap-then!
    state
    #(assoc % :next-up-fn stream-or-finalize-request)
-   #(stream-or-finalize-request state next-up evt val %)))
+   #(stream-or-finalize-request state evt val %)))
 
 (defn- handle-request
-  [state next-up _ [hdrs body :as request] _]
+  [state _ [hdrs body :as request] _]
   (let [keepalive?   (keepalive-request? request)
         expects-100? (expecting-100? request)
         head?        (= "HEAD" (request :request-method))
         chunked?     (= :chunked body)
-        next-up-fn
-        (cond
-         (not chunked?)  waiting-for-response
-         expects-100?    awaiting-100-continue
-         :else           stream-request-body)]
+        next-up-fn   (cond
+                      (not chunked?)  waiting-for-response
+                      expects-100?    awaiting-100-continue
+                      :else           stream-request-body)]
 
     (swap-then!
      state
@@ -134,32 +203,33 @@
         :head?      head?
         :next-up-fn next-up-fn)
      (fn [current-state]
-       (next-up :request request)
-       (finalize-exchange state current-state)))))
+       (maybe-finalize-exchange current-state)
+       ((.upstream current-state) :request request)
+       (maybe-close-connection current-state)))))
 
 (defn- mk-downstream-fn
-  [state next-dn]
+  [state]
   (fn [evt val]
     (if (#{:response :body} evt)
       (let [current-state @state]
-        ((.next-dn-fn current-state) state next-dn evt val current-state))
+        ((.next-dn-fn current-state) state evt val current-state))
       (next-dn evt val))))
 
 (defn- exchange
   [app dn conn]
-  (let [state   (atom (initial-exchange-state conn))
-        next-up (app (mk-downstream-fn state dn))]
+  (let [state   (atom (initial-exchange-state conn dn))
+        next-up (app (mk-downstream-fn state))]
+    ;; Track the upstream
+    (swap! state #(assoc % :upstream next-up))
+
     ;; The exchange event handler only processes events
     ;; related to the current HTTP exchange and will pass
     ;; on all other events.
     (fn [evt val]
       (if (#{:request :body} evt)
         (let [current-state @state]
-          ((.next-up-fn current-state) state next-up evt val current-state))
+          ((.next-up-fn current-state) state evt val current-state))
         (next-up evt val)))))
-
-(defrecord ConnectionState
-    [upstream])
 
 (defn- throw-http-pipelining-exception
   []
@@ -186,8 +256,12 @@
                (swap! state #(assoc % :upstream next-up))
                (next-up evt val)))
 
+           (= :close evt)
+           (when next-up
+             (throw (IOException. "Connection reset by peer")))
+
            next-up
            (next-up evt val)
 
-           :else
+           (not (#{:open :close} evt))
            (throw (Exception. "Not expecting a message right now."))))))))
