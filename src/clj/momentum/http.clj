4f93d63fb90a9b1df044c57d7cd001ca5be20c67
Carl Lerche <me@carllerche.com>
2012-03-07 19:17:34 -0800

    Remove an unneeded file

--- src/clj/momentum/http.clj
+++
@@ -1,8 +0,0 @@
-(ns momentum.http
-  (:use momentum.util.namespace)
-  (:require
-   [momentum.http.endpoint  :as endpoint]
-   [momentum.http.response  :as response]))
-
-(import-fn #'response/respond)
-(import-macro #'endpoint/endpoint)
