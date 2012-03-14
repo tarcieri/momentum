8ffe15f66004b4e674c4cdf03de4153c4f1eb899
Michael S. Klishin <michael@novemberain.com>
2012-03-14 12:57:41 +0400

    Back to 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.tilde.momentum/momentum "0.3.0-alpha1"
+(defproject io.tilde.momentum/momentum "0.3.0-SNAPSHOT"
   :description "Clojure library for high-performance server and client applications"
 
   :dependencies [[org.clojure/clojure "1.3.0"]]
